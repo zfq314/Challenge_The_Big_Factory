e1a66ecf62b9bce57ebe39ece15a310b8f4aa40a
zhaofuqiang <zfq0314@163.com>
2022-05-12 00:56:27 +0800

    commit

--- flinkcdc/src/main/java/com/bigdata/zfq/flinkcdc/FlinkCDC.java
+++ flinkcdc/src/main/java/com/bigdata/zfq/flinkcdc/FlinkCDC.java
@@ -1,10 +1,17 @@
 package com.bigdata.zfq.flinkcdc;
 
+
+import com.ververica.cdc.connectors.mysql.MySqlSource;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.DebeziumSourceFunction;
+import com.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment;
+//这块引入也出错，需要留意
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
  * TODO
@@ -14,7 +21,7 @@ import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment;
  */
 
 public class FlinkCDC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
@@ -28,11 +35,28 @@ public class FlinkCDC {
         //指定从CK自动重启策略
         env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 2000L));
         //设置状态后端
-        env.setStateBackend(new FsStateBackend("hdfs://hadoop102:8020/flinkCDC"));
+        //客户端访问集群端口 9000 3.x 8020
+        env.setStateBackend(new FsStateBackend("hdfs://mycluster/flinkCDC"));
         //设置访问HDFS的用户名
         System.setProperty("HADOOP_USER_NAME", "root");
 
         //创建Flink-MySQL-CDC的Source
+        DebeziumSourceFunction<String> mysqlSource = MySqlSource.<String>builder()
+                .hostname("hadoop31")
+                .port(3306)
+                .username("root")
+                .password("hadoopdb-hadooponeoneone@dc.com.")
+                .databaseList("dolphinscheduler")
+                .tableList("dolphinscheduler.qrtz_scheduler_state") //可选配置项,如果不指定该参数,则会读取上一个配置下的所有表的数据，注意：指定的时候需要使用"db.table"的方式
+                .startupOptions(StartupOptions.initial())
+                .deserializer(new StringDebeziumDeserializationSchema())
+                .build();
+        DataStreamSource<String> mysqlDS = env.addSource(mysqlSource);
+        //打印数据
+        mysqlDS.print();
+
+        //执行任务
+        env.execute();
 
     }
 }
